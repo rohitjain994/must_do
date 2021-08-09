08e64dc4417ded9c38bf930262b4771e295a66ad
Rohit Jain <rohitjain@Rohits-MacBook-Pro.local>
2021-08-09 14:57:10 +0530

    new must_do

--- DP/unique-path-s-d.py
+++ DP/unique-path-s-d.py
@@ -34,3 +34,26 @@ class Solution:
                 else:
                     obstacleGrid[i][j]=0
         return obstacleGrid[m-1][n-1]
+
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        m,n=len(grid),len(grid[0])
+        zcnt,sx,sy=0,0,0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    sx=i
+                    sy=j
+                elif grid[i][j]==0:
+                    zcnt+=1
+        def dfs(x,y,cnt)-> int:
+            if x<0 or y<0 or x>=len(grid) or y>=len(grid[0]) or grid[x][y] == -1:
+                return 0
+            if grid[x][y]==2:
+                return cnt == -1
+            grid[x][y]=-1
+            cnt-=1
+            ans = dfs(x+1,y,cnt)+dfs(x,y+1,cnt)+dfs(x-1,y,cnt)+dfs(x,y-1,cnt)
+            grid[x][y]=0
+            return ans
+        return dfs(sx,sy,zcnt)
